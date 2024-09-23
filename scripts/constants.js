c6f54f030cc4c36b6a77ba2e8d0ac531314ecfac
Andriy Berzhavych <andriyberzhavich@gmail.com>
2024-09-23 17:07:56 +0300

    fix acceptance of invalid commit

---
+++ scripts/constants.js
@@ -0,0 +1,29 @@
+const forbiddenCommitMessages = [
+    'WIP',
+    'temp',
+    'fix',
+    'update',
+    'dummy',
+    'initial commit',
+    'bugfix',
+    'minor changes',
+    'refactor',
+    'chore',
+    'changes',
+    'remove unused code',
+    'code cleanup',
+    'test',
+    'some changes',
+    'fix typo',
+    'bump version',
+    'improvements',
+    'fix issue',
+    'this is a commit',
+    'another commit',
+    'updated files',
+  ];
+
+
+module.exports = {
+    forbiddenCommitMessages
+};
--- scripts/validate-commit-message.js
+++ scripts/validate-commit-message.js
@@ -1,59 +1,37 @@
-const { execSync } = require('child_process');
+const fs = require('fs');
+const {forbiddenCommitMessages} = require('./constants');
 
-const forbiddenMessages = [
-  'WIP',
-  'temp',
-  'fix',
-  'update',
-  'dummy',
-  'initial commit',
-  'bugfix',
-  'minor changes',
-  'refactor',
-  'chore',
-  'changes',
-  'remove unused code',
-  'code cleanup',
-  'test',
-  'some changes',
-  'fix typo',
-  'bump version',
-  'improvements',
-  'fix issue',
-  'this is a commit',
-  'another commit',
-  'updated files'
-];
+const MIN_WORD_AMOUNT = 2
 
-const validateCommitMessage = () => {
-  try {
-    const commitMessage = execSync('git log -1 --pretty=%B').toString().trim();
+const validateCommitMessage = (commitMessage) => {
+  const commitMessagePattern = /^.{1,60}$/;
+  const wordCount = commitMessage.split(/\s+/).length;
 
-    const commitMessagePattern = /^.{1,60}$/;
-    const wordCount = commitMessage.split(/\s+/).length;
-
-    if (forbiddenMessages.includes(commitMessage.toLowerCase())) {
-      console.error(`Error: Commit message "${commitMessage}" is forbidden.
-        Please avoid using generic messages such as: ${forbiddenMessages.join(', ')}.`);
-      process.exit(1);
-    }
-
-    if (wordCount < 2) {
-      console.error(`Error: Commit message "${commitMessage}" must contain at least 2 words.`);
-      process.exit(1);
-    }
-
-    if (commitMessagePattern.test(commitMessage)) {
-      console.log(`Commit message "${commitMessage}" is valid.`);
-    } else {
-      console.error(`Error: Commit message "${commitMessage}" is not valid.
-        Commit messages must be concise and ideally no more than 60 characters.`);
-      process.exit(1);
-    }
-  } catch (error) {
-    console.error('Error getting commit message:', error.message);
+  if (forbiddenCommitMessages.includes(commitMessage.toLowerCase())) {
+    console.error(`Error: Commit message "${commitMessage}" is forbidden.
+      Please avoid using generic messages such as: ${forbiddenMessages.join(', ')}.`);
     process.exit(1);
   }
+
+  if (wordCount < MIN_WORD_AMOUNT) {
+    console.error(`Error: Commit message "${commitMessage}" must contain at least ${MIN_WORD_AMOUNT} words.`);
+    process.exit(1);
+  }
+
+  if (!commitMessagePattern.test(commitMessage)) {
+    console.error(`Error: Commit message "${commitMessage}" is not valid.
+      Commit messages must be concise and ideally no more than 60 characters.`);
+    process.exit(1);
+  }
+
+  console.log(`Commit message "${commitMessage}" is valid.`);
 };
 
-validateCommitMessage();
+const commitMessageFile = process.argv[2];
+try {
+  const commitMessage = fs.readFileSync(commitMessageFile, 'utf8').trim();
+  validateCommitMessage(commitMessage);
+} catch (error) {
+  console.error('Error reading commit message:', error.message);
+  process.exit(1);
+}
