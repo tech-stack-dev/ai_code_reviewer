1347a28c920d9b6841e8e07eb96bca5381e210f8
Andriy Berzhavych <andriyberzhavich@gmail.com>
2024-09-23 16:51:35 +0300

    add forbidden commit messages

--- scripts/validate-commit-message.js
+++ scripts/validate-commit-message.js
@@ -1,20 +1,48 @@
 const { execSync } = require('child_process');
 
+const forbiddenMessages = [
+    'WIP',           
+    'temp',           
+    'fix',          
+    'update',        
+    'dummy',           
+    'initial commit',  
+    'bugfix',          
+    'minor changes',  
+    'refactor',      
+    'chore',         
+    'changes',       
+    'remove unused code', 
+    'code cleanup',
+    'test',          
+    'some changes',    
+    'fix typo',      
+    'bump version',
+    'improvements',    
+    'fix issue',      
+    'this is a commit',
+    'another commit',  
+    'updated files'    
+  ];
+  
+
 const validateCommitMessage = () => {
   try {
     const commitMessage = execSync('git log -1 --pretty=%B').toString().trim();
 
-    const commitMessagePattern = /^(AICR-\d+: .{1,60}|Initial commit)$/i;
+    const commitMessagePattern = /^.{1,60}$/; 
+
+    if (forbiddenMessages.includes(commitMessage.toLowerCase())) {
+      console.error(`Error: Commit message "${commitMessage}" is forbidden.
+        Please avoid using generic messages such as: ${forbiddenMessages.join(', ')}.`);
+      process.exit(1);
+    }
 
     if (commitMessagePattern.test(commitMessage)) {
       console.log(`Commit message "${commitMessage}" is valid.`);
     } else {
       console.error(`Error: Commit message "${commitMessage}" is not valid.
-        Commit messages must follow this format:
-        JIRA-ID: Brief description
-        Example: AICR-1234: Implement new feature
-        - The description should be concise and ideally no more than 60 characters.
-        - Use the imperative mood for the description.`);
+        Commit messages must be concise and ideally no more than 60 characters.`);
       process.exit(1);
     }
   } catch (error) {
