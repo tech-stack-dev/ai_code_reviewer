0225af82c23b67671b01d0548004a7eefbadbd3f
Andriy Berzhavych <andriyberzhavich@gmail.com>
2024-09-24 15:15:59 +0300

    feat: add github action config

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,29 @@
-const world = 'world';
+import { Octokit } from '@octokit/rest';
 
-export function hello(who: string = world): string {
-  return `Hello ${who}! `;
+const githubToken = process.env.GITHUB_TOKEN;
+const octokit = new Octokit({ auth: githubToken });
+
+async function run() {
+  const context = process.env.GITHUB_EVENT_NAME;
+
+  if (context === 'pull_request' || context === 'issue_comment') {
+    const pullRequest = JSON.parse(process.env.GITHUB_EVENT_BODY ?? '');
+    await commentOnPullRequest(pullRequest);
+  }
 }
+
+async function commentOnPullRequest(pullRequest: any) {
+  const { owner, repo, number } = pullRequest;
+
+  await octokit.issues.createComment({
+    owner,
+    repo,
+    issue_number: number,
+    body: "Testing a bot", 
+  });
+}
+
+run().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
